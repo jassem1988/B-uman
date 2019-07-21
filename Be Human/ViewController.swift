700b6a18442c126d32eab896ba9a081b13bb68ca
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-07-21 09:16:20 +0300

    Initial Commit

---
+++ Be Human/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Be Human
+//
+//  Created by Jassem Al-Buloushi on 7/21/19.
+//  Copyright © 2019 Jassem Al-Buloushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
