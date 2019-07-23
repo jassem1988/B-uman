18590d740dcd9222165abfb3d3ffe668c6577757
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-07-23 11:51:35 +0300

    - Rename viewController to GenderViewController and add QuestionsViewController - Connect gender buttons to segue and change nav bar tint color

---
+++ Be Human/GenderViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Be Human
+//
+//  Created by Jassem Al-Buloushi on 7/21/19.
+//  Copyright © 2019 Jassem Al-Buloushi. All rights reserved.
+//
+
+import UIKit
+
+class GenderViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        
+        
+    }
+    
+    //MARK: - Buttons Actions
+    
+    @IBAction func maleButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "genderChoice", sender: sender)
+        navigationController?.navigationBar.barTintColor = UIColor.red
+        
+    }
+    
+
+
+    @IBAction func femaleButtonPressed(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "genderChoice", sender: sender)
+        navigationController?.navigationBar.barTintColor = UIColor.green
+    }
+}
+
