18316d522d8f62bb33c5b9ff4b23a6dadbbead64
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-07-24 11:28:39 +0300

    Add new folder structure

---
+++ Source/Controllers/QuestionsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  QuestionsViewController.swift
+//  Be Human
+//
+//  Created by Jassem Al-Buloushi on 7/23/19.
+//  Copyright © 2019 Jassem Al-Buloushi. All rights reserved.
+//
+
+import UIKit
+
+class QuestionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
